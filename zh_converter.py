52a3ae1029d7dfc6694dbc8d0b83ca7cb17cc577
pshaung <speshiou@gmail.com>
2020-12-01 11:37:16 +0800

    Suppress pylint error for importing opencc

--- zh_converter.py
+++ zh_converter.py
@@ -4,7 +4,7 @@ import os, sys
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.join(ROOT_DIR, "pylib"))
 
-import opencc
+import opencc # pylint: disable=import-error
 converter = opencc.OpenCC('tw2sp.json')
 
 def cht_to_chs(src):
